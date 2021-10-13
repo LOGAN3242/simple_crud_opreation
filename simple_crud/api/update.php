f7b911c00de55ca684578b64575d7ecff235b3dc
LOGAN3242 <80542010+LOGAN3242@users.noreply.github.com>
2021-10-13 11:17:17 +0530

    Add files via upload

--- simple_crud/api/update.php
+++ simple_crud/api/update.php
@@ -2,7 +2,7 @@
 
     include('../includes/connection.php');
 
-   // http://localhost/simple_crud/api/update.php?cap_id=1&cap_name=sarvaiya&cap_phone=986543210&cap_email=oms@001gmail.com    
+       
     $cap_id=$_REQUEST['cap_id'];
     $cap_name=$_REQUEST['cap_name'];
     $cap_phone=$_REQUEST['cap_phone'];
